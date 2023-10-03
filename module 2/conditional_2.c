e8c462a9b1be4a3e44537d6f38ce9af496e617ca
shm_usuf <shmusuf@gmail.com>
2023-10-03 16:25:52 +0600

    module 2 done

---
+++ module 2/conditional_2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=100)
+    {
+        printf("burger khabo");
+    }
+    else if(tk>=50)
+    {
+        printf("fuska khabo");
+    }
+    else
+    {
+        printf("kisui khabo na");
+    }
+    return 0;
+}
