446f10cfccbb13a13524fe6aa4895817e6418e83
shm_usuf <shmusuf@gmail.com>
2023-10-16 11:13:20 +0600

    sum of arr added

--- module 7/sum_of_arr.c
+++ module 7/sum_of_arr.c
@@ -4,12 +4,12 @@ int main()
     int n;
     scanf("%d", &n);
     int arr[n];
-    int sum = 0;
+
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-
+    int sum = 0;
     for (int i = 0; i < n; i++)
     {
         sum += arr[i];
