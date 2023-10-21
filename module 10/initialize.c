fe73cd42dd151c8c56f8d0747a89a75828844fb2
shm_usuf <shmusuf@gmail.com>
2023-10-21 15:32:06 +0600

    string initialize and input

--- module 10/initialize.c
+++ module 10/initialize.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    // char name[] = "Shah Mohammad Usuf\0";
-    char nick[5] = "usuf";
-    // printf("%s", name);
-    printf("%s", nick);
+    char a[8] = "shmusuf";
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%c", a[i]);
+    // }
+    int sz = sizeof(a);
+    printf("%d\n", sz);
+    printf("%s", a);
     return 0;
 }
