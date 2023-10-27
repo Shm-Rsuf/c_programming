0b7ae2aaf484f919d2030bb36b7ea2245d6a0232
unknown <usuf.iu.cse16@gmail.com>
2023-10-27 10:01:52 +0600

    pattern testing added

--- module 13/pattern_3.c
+++ module 13/pattern_3.c
@@ -2,8 +2,8 @@
 int main(){
     int n,s,k ; 
     scanf("%d",&n);
-    s=n-1;
-    k=1;
+    s=0;
+    k=2*n-1;
     for(int i = 1; i<=n; i++){
         for(int j = 1;j<=s;j++){
             printf(" ");
@@ -12,8 +12,8 @@ int main(){
         for(int j = 1; j<=k;j++){
             printf("*");
         }
-        s--;
-        k+=2;
+        s++;
+        k-=2;
         printf("\n");
     }
     return 0;
